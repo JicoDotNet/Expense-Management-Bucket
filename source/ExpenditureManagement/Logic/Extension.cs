4f93593917c047aef7599de858c3b7d6e24b17ca
Soubhik Nandy <nandy.soubhik@live.co.uk>
2024-04-03 16:05:14 +0530

    Initial code form Azure DevOps Repo

---
+++ source/ExpenditureManagement/Logic/Extension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpenditureManagement
+{
+    public static class Extension
+    {
+        public static long TimeStamp(this DateTime DTobj)
+        {
+            try
+            {
+                return ((DTobj.Ticks - 621355968000000000) / 10000);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+
+    public class GenericLogic
+    {
+        public static DateTime IstNow
+        {
+            get
+            {
+                return DateTime.UtcNow.AddMinutes(330);
+            }
+        }
+    }
+}
---
+++ source/ExpenditureManagement/Models/Expenses.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpenditureManagement.Models
+{
+    public class Expenses : TableEntity
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string Details { get; set; }
+        public double Amount { get; set; }
+        public string TransactionType { get; set; }
+        public string ExpensesType { get; set; }
+        public string Note { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class ExpensesType : TableEntity
+    {
+        public string ExpensesTypeName { get; set; }
+    }
+
+    public class TransactionType : TableEntity
+    {
+        public string TransactionTypeName { get; set; }
+    }
+
+    public class LoginCred: TableEntity
+    {
+        public string UserName { get; set;}
+        public string Password { get; set;}
+        public bool IsActive { get; set; }
+    }
+}
